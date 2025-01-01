2309aaa9582965918e3790fa01404b092ffa6d79
NightoEys <121339950+NightEye000@users.noreply.github.com>
2025-01-01 22:27:46 +0700

    Add files via upload

---
+++ DatabaseLocalWithDao/Barang.kt
@@ -0,0 +1,22 @@
+package com.example.latihanresponsi
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity
+data class Barang(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name="id")
+    var id:Int = 0,
+
+    @ColumnInfo(name = "nama")
+    var nama:String? = null,
+
+    @ColumnInfo(name = "jenis")
+    var jenis:String? = null,
+
+    @ColumnInfo(name = "harga")
+    var harga:Int? = null,
+)
